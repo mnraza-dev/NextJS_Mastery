ef8ffafc9f4abcc05d0a9a4260c2389ac17aee06
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-23 15:54:52 +0530

    Implement form submission handling in CreateSnippetPage. Updated layout to include a form element for better user interaction and added a submit handler for the snippet creation process.

--- code-snippets/src/app/snippets/new/page.tsx
+++ code-snippets/src/app/snippets/new/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -5,9 +6,13 @@ import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const CreateSnippetPage = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log('Form submitted')
+    }
     return (
         <div className='h-screen flex justify-center items-center'>
-            <div className='m-5  flex flex-col gap-2 p-5 rounded-md  shadow-xl w-1/2'>
+            <form onSubmit={handleSubmit} className='m-5  flex flex-col gap-2 p-5 rounded-md  shadow-xl w-1/2'>
                 <h1 className='text-2xl font-bold text-center'>Create Snippet</h1>
                 <div className='flex flex-col gap-2'>
                     <Label>Title</Label>
@@ -15,8 +20,8 @@ const CreateSnippetPage = () => {
                 <div className='flex flex-col gap-2'>
                     <Label>Code</Label>
                     <Textarea placeholder='Code here...' /></div>
-                <Button className='mt-5' title='Create Snippet' >Create Snippet</Button>
-            </div>
+                <Button type='submit' className='mt-5' title='Create Snippet' >Create Snippet</Button>
+            </form>
         </div>
     )
 }

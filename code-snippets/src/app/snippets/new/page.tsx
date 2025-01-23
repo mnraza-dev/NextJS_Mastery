7370abaa505136967d0b5675ceb1b16cf0f9ecc2
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-23 08:24:50 +0530

    Enhance CreateSnippetPage layout with improved styling and structure. Updated the component to use a flexbox layout for better responsiveness, added padding and shadow for visual appeal, and modified input placeholders for clarity.

--- code-snippets/src/app/snippets/new/page.tsx
+++ code-snippets/src/app/snippets/new/page.tsx
@@ -6,13 +6,17 @@ import React from 'react'
 
 const CreateSnippetPage = () => {
     return (
-        <div>
-            <h1>Create Snippet</h1>
-            <div>   <Label>Title</Label>
-                <Input type='text' placeholder='Title' /></div>
-            <div>     <Label>Code</Label>
-                <Textarea placeholder='Code' /></div>
-            <Button className='mt-5' title='Create Snippet' >Create Snippet</Button>
+        <div className='h-screen flex justify-center items-center'>
+            <div className='m-5  flex flex-col gap-2 p-5 rounded-md  shadow-xl w-1/2'>
+                <h1 className='text-2xl font-bold text-center'>Create Snippet</h1>
+                <div className='flex flex-col gap-2'>
+                    <Label>Title</Label>
+                    <Input type='text' placeholder='add a Title here...' /></div>
+                <div className='flex flex-col gap-2'>
+                    <Label>Code</Label>
+                    <Textarea placeholder='Code here...' /></div>
+                <Button className='mt-5' title='Create Snippet' >Create Snippet</Button>
+            </div>
         </div>
     )
 }

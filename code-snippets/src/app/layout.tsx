7631780626b78e2033193cade0c351b55cd67406
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-23 08:07:37 +0530

    Add Snippets model to Prisma schema, enhance layout with responsive container, and update HomePage for improved UI

--- code-snippets/src/app/layout.tsx
+++ code-snippets/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+          {children}
+        </div>
       </body>
     </html>
   );
--- code-snippets/src/app/page.tsx
+++ code-snippets/src/app/page.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-       <Button>Click me</Button>
+    <div className="flex p-5 justify-between h-screen">
+      <h1>Code <span className="text-red-500">Snippets</span> </h1>
+      <Button>Add Snippet</Button>
     </div>
   )
 }

cd5b0740e5d697ee69f2b5d1147f87612dac58fd
mnraza-dev <mnraza94@gmail.com>
2025-03-05 23:10:40 +0530

    Enhance home page with a new description for the dashboard

--- demos/src/app/page.tsx
+++ demos/src/app/page.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col items-center justify-center p-6 '>
-    
-    <div className="text-center">
-      <h1 className='text-5xl mb-6 font-bold text-gray-800'>
-        Welcome to the Dashboard App
-      </h1>
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col items-center justify-center p-6 ">
+      <div className="text-center">
+        <h1 className="text-5xl mb-6 font-bold text-gray-800">
+          Welcome to the Dashboard App
+        </h1>
+        <p className="text-lg text-gray-400 mb-8">
+          Manage your tasks, track analytics, and stay organized with our
+          powerful dashboard.
+        </p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

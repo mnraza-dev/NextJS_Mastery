5faf19baf68b40522f4ae127e174f747459f8599
mnraza-dev <mnraza94@gmail.com>
2025-03-05 23:18:46 +0530

    Enhance dashboard layout with gradient background and improved styling

--- demos/src/app/dashboard/page.tsx
+++ demos/src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="p-8 bg-gradient-to-br from-blue-50 to-indigo-100 min-h-screen">
+      <div className="max-w-6xl bg-white rounded-2xl shadow-lg p-8 space-y-8  mx-auto">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl mb-5 sm:mb-0 font-extrabold text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

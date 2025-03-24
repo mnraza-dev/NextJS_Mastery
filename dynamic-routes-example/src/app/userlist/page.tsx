ef06db91026751070b9cf62e9d374575aed2e458
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-25 00:59:30 +0530

    Add UserList page and update Home page with navigation to User List

---
+++ dynamic-routes-example/src/app/userlist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserList = () => {
+    return (
+        <div className='flex flex-col justify-center items-center m-4'>
+            <h1 className='text-4xl text-center'>All Users List</h1>
+        </div>
+    )
+}
+
+export default UserList

89f4f086a1c9d8b8b389d68d401e0274afb2aa59
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-24 23:42:17 +0530

    Initialize Next.js project with Tailwind CSS, add basic layout and pages

---
+++ challenge-01/src/app/products/allproducts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AllProducts: React.FC = () => {
+    return (
+        <div><h1>
+            (1200) products in the store
+        </h1>
+
+        </div>
+    )
+}
+
+export default AllProducts
---
+++ challenge-01/src/app/products/specificproduct/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SpecificProduct: React.FC = () => {
+    return (
+        <div>SpecificProduct</div>
+    )
+}
+
+export default SpecificProduct

3253ec0e21194566c33625e39c6c2f8461337d3f
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-25 00:38:09 +0530

    Refactor navigation in Home page to use a reusable navigate function for improved code clarity

--- challenge-03/src/app/page.tsx
+++ challenge-03/src/app/page.tsx
@@ -5,14 +5,20 @@ import React from 'react'
 
 const Home = () => {
   const router = useRouter()
+
+  const navigate = (page: string) => {
+    router.push(`/${page}`);
+  }
   return (
     <div>
 
       <nav className='max-w-full text-black bg-amber-500 text-2xl'>
         <div className='flex gap-2 p-4'>
 
-          <div className='cursor-pointer'>
-            <button onClick={() => router.push('/login')}>login</button></div>
+        
+          <button className='cursor-pointer' onClick={() => navigate('colors')}>colors</button>
+          <button className='cursor-pointer' onClick={() => navigate('login')}>login</button>
+
         </div>
       </nav>
 

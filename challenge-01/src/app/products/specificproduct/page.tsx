28bc28b29504d505609bb5bc9f8483c05ad8072b
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-24 23:44:06 +0530

    Update SpecificProduct component to display product title

--- challenge-01/src/app/products/specificproduct/page.tsx
+++ challenge-01/src/app/products/specificproduct/page.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const SpecificProduct: React.FC = () => {
     return (
-        <div>SpecificProduct</div>
+        <div>F
+            <h1>Here is my one and only product</h1>
+        </div>
     )
 }
 

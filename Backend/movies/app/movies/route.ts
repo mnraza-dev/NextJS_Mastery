8a779b444cd05fcda6054e8c253768512ff43237
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-27 15:42:20 +0530

    fix: add missing newline at end of file in route.ts

--- Backend/movies/app/movies/route.ts
+++ Backend/movies/app/movies/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
 import { movies } from "./db";
+
 export async function GET(req: Request) {
-    return NextResponse.json(movies);                                       
+    return NextResponse.json(movies);
+}
+
+export async function POST(req: Request) {
+    const movie = await req.json();
+    const newMovie = {
+        ...movie
+    }
+    movies.push(newMovie);
+    return new Response(JSON.stringify(newMovie));
 }

ca115bebbd3246786e66138505e6a6cfde3398ec
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-02 23:39:01 +0530

    Add PostCSS and TypeScript configuration files for demo project
    - Created postcss.config.mjs to configure PostCSS with Tailwind CSS.
    - Added tsconfig.json for TypeScript compiler options and project settings.

---
+++ Animations/demos/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import motion from 'motion/react'
+const Home = () => {
+  return (
+    <div>
+      <div className="box">
+        hi
+      </div>
+    </div>
+  )
+}
+
+export default Home

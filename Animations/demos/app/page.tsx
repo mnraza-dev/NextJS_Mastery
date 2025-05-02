7fe0422841059de3fc8593b0f413ebb44cee61c0
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-02 23:40:25 +0530

    motion.div added

--- Animations/demos/app/page.tsx
+++ Animations/demos/app/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import motion from 'motion/react'
+import {motion} from 'motion/react'
 const Home = () => {
   return (
     <div>
       <div className="box">
-        hi
+        <motion.div
+          className="box"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          Hello world
+        </motion.div>
       </div>
     </div>
   )

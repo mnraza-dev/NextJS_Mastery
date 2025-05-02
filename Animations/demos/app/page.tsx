1b3c0c8768e70f612d7f035f7fcddc1d4a1f69c5
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-02 23:48:08 +0530

    refactor: simplify motion.div usage and enhance animation properties

--- Animations/demos/app/page.tsx
+++ Animations/demos/app/page.tsx
@@ -4,16 +4,14 @@ import * as motion from 'motion/react-client'
 const Home = () => {
   return (
     <div>
-      <div className="box">
+      
         <motion.div
           className="box"
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          animate={{ opacity: 1 , x:20, y:20 }}
           transition={{ duration: 1 }}
-        >
-          Hello
-        </motion.div>
-      </div>
+        />
+    
     </div>
   )
 }

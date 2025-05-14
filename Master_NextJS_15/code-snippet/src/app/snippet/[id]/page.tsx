bc634729d792cad7881426726296ed3c47d95ac1
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-15 03:16:15 +0530

    feat: add edit and delete icons to SnippetDetailPage buttons and improve layout

--- Master_NextJS_15/code-snippet/src/app/snippet/[id]/page.tsx
+++ Master_NextJS_15/code-snippet/src/app/snippet/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { prisma } from '@/lib/prisma'
-import { CopyIcon } from 'lucide-react'
+import { CopyIcon, EditIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 interface SnippetDetailPageProps {
     params: Promise<{ id: string }>
@@ -20,25 +20,25 @@ const SnippetDetailPage = async ({ params, }: SnippetDetailPageProps) => {
         )
     }
     return (
-        <div className='flex flex-col h-full'>
-            <div className='flex flex-col h-full'>
+        <div className='flex flex-col h-screen '>
+            <div className='flex flex-col flex-grow'>
                 <div>
                     <div className=' flex items-center justify-between bg-amber-300 shadow-md rounded-2xl py-8 px-16 hover:shadow-lg transition-shadow duration-300'>
                         <h1 className='text-3xl font-light ' > {res?.title}</h1>
 
                         <div className='flex gap-2'>
                             <Button variant={'outline'} className='bg-black/80 text-white hover:bg-black/90 cursor-pointer' size={'lg'} >
-                                Edit
+                               <EditIcon className='w-4 h-4 mr-2' /> Edit
                             </Button>
                             <Button className='bg-red-500 text-white hover:bg-red-600 cursor-pointer' size={'lg'} >
-                                Delete
+                                <Trash2Icon className='w-4 h-4 mr-2' /> Delete
                             </Button>
                         </div>
                     </div>
                     <hr className='my-4 border-2 border-amber-200' />
 
                 </div>
-                <div className='h-[58vh] overflow-auto relative'>
+                <div className='h-full overflow-auto relative p-4 bg-amber-100'>
                     <CopyIcon className='absolute top-4 right-4 w-6 h-6 cursor-pointer text-gray-600 hover:text-green-600' />
                     <pre className='text-gray-600'>{res?.code}</pre>
                 </div>

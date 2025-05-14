c57607824f085139232021749285e29ea8d06b0c
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-14 23:33:33 +0530

    refactor: update headings in Home component for consistency

--- Master_NextJS_15/code-snippet/src/app/page.tsx
+++ Master_NextJS_15/code-snippet/src/app/page.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div className=''>
+      <h1 className='text-4xl font-semibold'>  Home</h1>
+
       <div className='flex justify-between items-center'>
-        <h1>  Snippets App</h1>
+        <h1 className='text-lg font-light'>  Snippets</h1>
         <Button variant='outline' className='cursor-pointer' >
           Create Snippet
         </Button>

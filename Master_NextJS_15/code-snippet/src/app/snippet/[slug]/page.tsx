3717912a51694818165b7d0e8b18c045039f3b91
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-15 02:21:01 +0530

    feat: add SnippetDetailPage component for displaying snippet details

---
+++ Master_NextJS_15/code-snippet/src/app/snippet/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SnippetDetailPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-full'>
+      <h1 className='text-2xl font-semibold'> Snippet Detail</h1>
+        <div className='flex flex-col items-center justify-center h-full'>
+           
+        </div>
+
+
+    </div>
+  )
+}
+
+export default SnippetDetailPage

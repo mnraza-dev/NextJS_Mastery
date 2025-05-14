973a18069fd4fad54131d282fa903dd580549c40
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-15 02:14:13 +0530

    fix: update Home component layout and improve snippet display with code and copy icon

--- Master_NextJS_15/code-snippet/src/app/page.tsx
+++ Master_NextJS_15/code-snippet/src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { prisma } from '@/lib/prisma'
+import { CopyIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const Home = async () => {
   const snippets = await prisma.code_snippet.findMany();
   console.log('Snippets', snippets)
-
   return (
     <div className=''>
       <h1 className='text-4xl font-semibold'>  Home</h1>
@@ -16,7 +16,7 @@ const Home = async () => {
           <Button size={'lg'} className='cursor-pointer mt-4 bg-amber-300 w-full'> Create Snippet</Button>
         </Link>
       </div>
-      <div className='flex flex-col items-center justify-center h-[80vh] '>
+      <div className='flex flex-col items-center justify-center '>
         {
           !snippets.length ? (
             <div className='flex flex-col items-center justify-center h-full'>
@@ -28,9 +28,14 @@ const Home = async () => {
           ) : (
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
               {snippets.map((snippet) => (
-                <Link href={`/snippet/${snippet.id}`} key={snippet.id} className='bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300'>
-                  <h2 className='text-xl font-semibold'>{snippet.title}</h2>
-                  <p className='text-gray-600'>{snippet.description}</p>
+                <Link href={`/snippet/${snippet.id}`} key={snippet.id} className='bg-white shadow-md rounded-2xl p-4 hover:shadow-xl transition-shadow duration-300'>
+<div className='flex items-center justify-between'>
+                    <h2 className='text-xl text-green-800 font-semibold'>{snippet.title}</h2>
+                  <CopyIcon className='w-5 h-5 text-gray-800' />
+</div>
+                  <div className='h-[200px] overflow-auto'>
+                    <pre className='text-gray-600'>{snippet.code}</pre>
+                  </div>
                 </Link>
               ))}
             </div>

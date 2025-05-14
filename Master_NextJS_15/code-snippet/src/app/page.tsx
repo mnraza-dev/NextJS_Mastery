b70c8b220924c5c975eede52b27f64108f0b9a4c
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-15 02:19:33 +0530

    fix: update snippet display to improve layout and remove commented code

--- Master_NextJS_15/code-snippet/src/app/page.tsx
+++ Master_NextJS_15/code-snippet/src/app/page.tsx
@@ -29,21 +29,20 @@ const Home = async () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
               {snippets.map((snippet) => (
                 <Link href={`/snippet/${snippet.id}`} key={snippet.id} className='bg-white shadow-md rounded-2xl p-4 hover:shadow-xl transition-shadow duration-300'>
-<div className='flex items-center justify-between'>
-                    <h2 className='text-xl text-green-800 font-semibold'>{snippet.title}</h2>
-                  <CopyIcon className='w-5 h-5 text-gray-800' />
-</div>
-                  <div className='h-[200px] overflow-auto'>
-                    <pre className='text-gray-600'>{snippet.code}</pre>
+                  <div className='flex items-center justify-between'>
+                    <h2 className='text-xl text-gray-800 font-semibold'>{snippet.title}</h2>
+                    {/* <CopyIcon className='w-5 h-5 text-gray-600' /> */}
                   </div>
+                  <Button variant='link' className='mt-2 w-full cursor-pointer '>View </Button>
+                  {/* <div className='h-[200px] overflow-auto'>
+                    <pre className='text-gray-600'>{snippet.code}</pre>
+                  </div> */}
                 </Link>
               ))}
             </div>
           )
         }
-
       </div>
-
     </div>
   )
 }

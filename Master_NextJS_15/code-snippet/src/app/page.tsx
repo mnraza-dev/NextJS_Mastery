35bf0ffb9cd2de8e6ec38059640ae9082b3af0b4
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-15 15:34:24 +0530

    refactor: clean up Home component by removing unused imports and unnecessary whitespace

--- Master_NextJS_15/code-snippet/src/app/page.tsx
+++ Master_NextJS_15/code-snippet/src/app/page.tsx
@@ -1,14 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { prisma } from '@/lib/prisma'
-import { CopyIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
-
 const Home = async () => {
   const snippets = await prisma.code_snippet.findMany();
   console.log('Snippets', snippets)
   return (
-    <div className=''>
+    <div>
       <h1 className='text-4xl font-semibold'>  Home</h1>
       <div className='flex justify-between items-center'>
         <h1 className='text-lg font-light'>  Snippets</h1>
@@ -29,14 +27,8 @@ const Home = async () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
               {snippets.map((snippet) => (
                 <Link href={`/snippet/${snippet.id}`} key={snippet.id} className='bg-amber-300 hover:bg-amber-400 shadow-md rounded-2xl py-8 px-16 hover:shadow-lg transition-shadow duration-300'>
-
-                    <h2 className='text-lg  text-black font-semibold'>{snippet.title}</h2>
-        
-
+                  <h2 className='text-lg  text-black font-semibold'>{snippet.title}</h2>
                   <Button variant='link' className='mt-2 w-full cursor-pointer text-black '>View </Button>
-
-
-
                 </Link>
               ))}
             </div>
@@ -46,5 +38,4 @@ const Home = async () => {
     </div>
   )
 }
-
 export default Home
